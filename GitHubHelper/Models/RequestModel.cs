fafb55c85cc0af57c7cf779bb99c47b95d5805dd
Artem Prokofiev <artem-prokofiev@yandex.ru>
2022-11-05 20:23:48 +0400

    added setting httpClient

---
+++ GitHubHelper/Models/RequestModel.cs
@@ -0,0 +1,11 @@
+﻿namespace GitHubHelper.Models{
+    public class RequestModel
+    {
+        public string? RepositoryName {get; set;}
+        public string? AuthorName {get; set;}
+        public string? DiscussionName {get; set;}
+        public int CommentsQuontity {get; set;}
+        
+    }
+}
+
--- GitHubHelper/Program.cs
+++ GitHubHelper/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System.Net.Http.Headers;
+using GitHubHelper.Models;
+
+RequestModel requestModel = new RequestModel();
+var repos = requestModel.RepositoryName;
+var owner = requestModel.AuthorName;
+var httpClient = new HttpClient();
+httpClient.DefaultRequestHeaders.UserAgent.Add(
+        new ProductInfoHeaderValue("GitHubHelper", "1"));
+Console.WriteLine("Enter owner name: ");
+Console.WriteLine("Enter repos name: ");
+var contentUrl = $"https://api.github.com/repos/{owner}/{repos}";
+
+
+ 
